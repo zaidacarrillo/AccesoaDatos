10fc2332626cb57143690a792b7be9dda5f0ff7e
zaidacarrillo <zaidacarrillovarela@gmail.com>
2020-11-17 14:34:48 +0100

    .

--- EjercicioJDBC/src/capaDatos/AsignaturaDAO.java
+++ EjercicioJDBC/src/capaDatos/AsignaturaDAO.java
@@ -5,239 +5,46 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import capaModelo.IAsignaturaDAO;
 import capaModelo.ICicloDAO;
 import capaModelo.Entidades.Asignatura;
 import capaModelo.Entidades.Ciclo;
 
-public class AsignaturaDAO implements ICicloDAO {
+public class AsignaturaDAO implements IAsignaturaDAO {
 
 	@Override
-	public void insertar(Ciclo ciclo) {
-		ConexionMySQL con = new ConexionMySQL();
+	public void insertar(Asignatura asignatura) {
 
-		Connection c = con.creacionConexion();
-
-		String consulta = "INSERT INTO CICLO" 
-						+ "(NOMBRE, GRADO) VALUES" 
-						+ "(?, ?);";
-		try {
-			PreparedStatement ps = c.prepareStatement(consulta);
-
-			ps.setString(1, ciclo.getNombre());
-			ps.setString(2, ciclo.getGrado());
-			ps.addBatch();
-			ps.executeBatch();
-
-		} catch (SQLException e) {
-			System.out.println("Error en la ejecución de la consulta insertar objeto ciclo.");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (c != null && !c.isClosed()) {
-					con.cerrarConexion(c);
-				}
-			} catch (SQLException e) {
-				System.out.println("No se pudo cerrar la conexión.");
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@Override
-	public void insertarMultiple(ArrayList<Ciclo> listaCiclos) {
-		ConexionMySQL con = new ConexionMySQL();
-		Connection c = con.creacionConexion();
-
-		String consulta = "INSERT INTO CICLO" 
-						+ "(NOMBRE, GRADO) VALUES" 
-						+ "(?, ?);";
-		try {
-			PreparedStatement psMultiple = c.prepareStatement(consulta);
-			for (Ciclo ci : listaCiclos) {
-				psMultiple.setString(1, ci.getNombre());
-				psMultiple.setString(2, ci.getGrado());
-				psMultiple.addBatch();
-			
-
-			}
-
-			psMultiple.executeBatch();
-		} catch (SQLException e) {
-			System.out.println("Error en la ejecución de la consulta insertar múltiples objetos de tipo ciclo.");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (c != null && !c.isClosed()) {
-					con.cerrarConexion(c);
-				}
-			} catch (SQLException e) {
-				System.out.println("No se pudo cerrar la conexión.");
-				e.printStackTrace();
-			}
-		}
-
-	}
-
-	@Override
-	public void eliminar(Ciclo ciclo) {
-		ConexionMySQL con = new ConexionMySQL();
-		Connection c = con.creacionConexion();
-
-		String consulta = "DELETE FROM CICLO WHERE ID = ?" ;
-		try {
-			PreparedStatement psEliminar = c.prepareStatement(consulta);
-			psEliminar.setInt(1, ciclo.getId());
-			psEliminar.addBatch();
-			psEliminar.executeBatch();
-			
-		} catch (SQLException e) {
-			System.out.println("Error en la ejecución de la consulta borrar registros de tipo id.");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (c != null && !c.isClosed()) {
-					con.cerrarConexion(c);
-				}
-			} catch (SQLException e) {
-				System.out.println("No se pudo cerrar la conexión.");
-				e.printStackTrace();
-			}
-		}
-						
-		
-
-	}
-
-	@Override
-	public void eliminarMultiple(ArrayList<Ciclo> listaCiclos) {
-		ConexionMySQL con = new ConexionMySQL();
-		Connection c = con.creacionConexion();
-
-		String consulta = "DELETE FROM CICLO WHERE ID = ?" ;
-		try {
-			PreparedStatement pseliminarMultiples = c.prepareStatement(consulta);
-			for(Ciclo ci : listaCiclos) {
-				pseliminarMultiples.setInt(1, ci.getId());
-				pseliminarMultiples.addBatch();
-			}
-			
-			pseliminarMultiples.executeBatch();
-		} catch (SQLException e) {
-			System.out.println("Error en la ejecución de la consulta borrar múltiples registros de tipo id.");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (c != null && !c.isClosed()) {
-					con.cerrarConexion(c);
-				}
-			} catch (SQLException e) {
-				System.out.println("No se pudo cerrar la conexión.");
-				e.printStackTrace();
-			}
-		}
-
-	}
-
-	@Override
-	public void modificar(Ciclo ciclo) {
-		ConexionMySQL con = new ConexionMySQL();
-		Connection c = con.creacionConexion();
-		
-		String consulta = "UPDATE CICLO SET nombre = ?, grado = ? WHERE id = ?";
-		try {
-			PreparedStatement psmodificar = c.prepareStatement(consulta);
-			psmodificar.setString(1, ciclo.getNombre());
-			psmodificar.setString(2, ciclo.getGrado());
-			psmodificar.setInt(3, ciclo.getId());
-			psmodificar.addBatch();
-			psmodificar.executeBatch();
-			
-		} catch (SQLException e) {
-			System.out.println("Error en la ejecución de la consulta modificar un registro de tipo id.");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (c != null && !c.isClosed()) {
-					con.cerrarConexion(c);
-				}
-			} catch (SQLException e) {
-				System.out.println("No se pudo cerrar la conexión.");
-				e.printStackTrace();
-			}
-		}
-
-	}
-
-	@Override
-	public void modificarMultiple(ArrayList<Ciclo> listaCiclos) {
-		ConexionMySQL con = new ConexionMySQL();
-		Connection c = con.creacionConexion();
-		
-		String consulta = "UPDATE CICLO SET nombre = ?, grado = ? WHERE id = ?";
-		try {
-			PreparedStatement psmodificarMultiple = c.prepareStatement(consulta);
-			for(Ciclo ci : listaCiclos) {
-				psmodificarMultiple.setString(1, ci.getNombre());
-				psmodificarMultiple.setString(2, ci.getGrado());
-				psmodificarMultiple.setInt(3, ci.getId());
-				psmodificarMultiple.addBatch();
-			}
-			
-			psmodificarMultiple.executeBatch();
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		} finally {
-			try {
-				if (c != null && !c.isClosed()) {
-					con.cerrarConexion(c);
-				}
-			} catch (SQLException e) {
-				System.out.println("No se pudo cerrar la conexión.");
-				e.printStackTrace();
-			}
-		}
 		
 	}
 
 	@Override
-	public void crearCicloAsignaturas(Ciclo ciclo, ArrayList<Asignatura> listaAsignaturas) {
-		ConexionMySQL con = new ConexionMySQL();
-		Connection c = con.creacionConexion();
-		try {
-			c.setAutoCommit(false);
-			//INSERCIÓN OBJETO CICLO EN TABLA CICLO 
-			String consulta = "INSERT INTO CICLO (NOMBRE, GRADO) VALUES (?,?);";
-			PreparedStatement ps = c.prepareStatement(consulta);
-			ps.setString(1, ciclo.getNombre());
-			ps.setString(2, ciclo.getGrado());
+	public void insertarMultiple(ArrayList<Asignatura> listaAsignaturas) {
+
 		
-			ps.executeUpdate();
-			//Obtencion id ciclo
-			
-			
-			//INSERCION ASIGNATURAS
-			String consulta2 = "INSERT INTO ASIGNATURAS (ID, NOMBRE, HORAS) VALUES (?,?);";
-			PreparedStatement ps2 = c.prepareStatement(consulta);
-			
+	}
+
+	@Override
+	public void eliminar(Asignatura asignatura) {
+
 		
-			for(Asignatura a : listaAsignaturas) {
-				ps2.setInt(1, ciclo.getId());
-				ps2.setString(2, a.getNombre());
-				ps2.setInt(3, a.getHorasSemanales());
-				ps2.addBatch();
-			}
-			ps2.executeBatch();
-			
-			c.commit();
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		}
+	}
+
+	@Override
+	public void eliminarMultiple(ArrayList<Asignatura> listaAsignaturas) {
+
 		
+	}
+
+	@Override
+	public void modificar(Asignatura asignatura) {
+
 		
-				
-		
+	}
+
+	@Override
+	public void modificarMultiple(ArrayList<Asignatura> listaAsignaturas) {
+	
 		
 	}
 
